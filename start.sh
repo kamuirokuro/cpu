38b634c28548063891e063ab0fc8f79e8b6087b9
kamuirokuro <79147759+kamuirokuro@users.noreply.github.com>
2021-02-18 15:22:34 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DDf4DpCBPQaCxAVs4yuB4o5Km2vhkxG7yW -p c=DOGE -t 5
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DDf4DpCBPQaCxAVs4yuB4o5Km2vhkxG7yW -p c=DOGE -t 2
 
 
 done
